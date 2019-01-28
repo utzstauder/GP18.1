77be918dfc9840865f77bba9c5736345a8bd5e16
Utz Stauder <utzstauder@users.noreply.github.com>
2019-01-28 15:16:01 +0100

    Added SpawnObjects.cs

---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour {
+
+    [SerializeField] GameObject prefab;
+    [SerializeField] int amount;
+    [SerializeField] int amountPerRow;
+    [SerializeField] Vector3 rowOffset;
+    [SerializeField] Vector3 columnOffset;
+
+	// Use this for initialization
+	void Start () {
+        Spawn();
+	}
+	
+    void Spawn()
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject newObject = Instantiate(prefab, transform);
+            newObject.transform.localPosition =
+                rowOffset * (i % amountPerRow) +
+                columnOffset * (i / amountPerRow);
+        }
+    }
+}
