2c40699a9f5b64311f88e1a27dc49bf4f6f21b80
Utz Stauder <utzstauder@users.noreply.github.com>
2019-01-28 16:43:53 +0100

    Implemented IDamagable interface

---
+++ Assets/Scripts/Camera/ShootFromCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootFromCamera : MonoBehaviour {
+
+    Camera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    void Update () {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }	
+	}
+
+    void Shoot()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            Debug.Log(hitInfo.collider.gameObject.name);
+
+            IDamagable target = hitInfo.collider.gameObject.GetComponent<IDamagable>();
+            if (target != null)
+            {
+                target.DoDamage(1);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCube : MonoBehaviour, IDamagable {
+
+    [SerializeField] int maxHp;
+    int currentHp;
+
+    void Start () {
+        currentHp = maxHp;
+	}
+
+    void Defeat()
+    {
+        Destroy(gameObject);
+    }
+
+    public void DoDamage(int damageAmount)
+    {
+        currentHp -= damageAmount;
+
+        if (currentHp <= 0)
+        {
+            Defeat();
+        }
+
+        // TODO: cooldown / invincibility time
+    }
+}
